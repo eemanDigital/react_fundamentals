a495a5b17665212ddae49b502d3a8dd9fa3451ad
eemanDigital <eemandigitalconcept@gamil.com>
2023-09-02 10:19:36 +0100

    React Fundamentals

---
+++ src/books.js
@@ -0,0 +1,28 @@
+import img1 from "./images/book.jpg"; //import from the source file
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg"
+
+export const books = [
+  {
+    title: "Lion's Den",
+    author: "John Smilger",
+    image: img1,
+    id: 1,
+  },
+
+  {
+    title: "Prince of Alaba",
+    author: "Ademola Cocker",
+    image: img2,
+    id: 2,
+  },
+
+  {
+    title: "BILLIONAIRE HABITS FOR ENTREPRENEURS",
+    author: "DR. STEPHEN AKINTAYO",
+    image: img3,
+    id:4,
+  },
+];
+//
+// by  (Author)
